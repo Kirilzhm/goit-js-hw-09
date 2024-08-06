7c03c5697e3419a34e6e7453fbe14e46ac779600
Kiril <Kirilzhm@gmail.com>
2024-08-06 15:58:27 +0200

    attempt to remove The error

--- src/js/2-gallery.js
+++ src/js/2-gallery.js
@@ -6,3 +6,8 @@ const formData = {
 const form = document.querySelector(`.feedback-form`);
 const emailInput = form.querySelector(`input[name="email"]`);
 const messageInput = form.querySelector(`textarea[name="message"]`);
+
+function saveToLocalStorage() {
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+};
+
