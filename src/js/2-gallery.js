af46c9807c3837d5852a81b043c81767e7e95bef
Kiril <Kirilzhm@gmail.com>
2024-08-06 15:28:21 +0200

    attempt to remove the error

---
+++ src/js/2-gallery.js
@@ -0,0 +1,8 @@
+const formData = {
+    email: "",
+    message: ""
+};
+
+const form = document.querySelector(`.feedback-form`);
+const emailInput = form.querySelector(`input[name="email"]`);
+const messageInput = form.querySelector(`textarea[name="message"]`);
