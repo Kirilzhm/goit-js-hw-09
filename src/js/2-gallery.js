ba2d0b34e58f04e2f5b7f4d94208f1056d2a8bd7
Kiril <Kirilzhm@gmail.com>
2024-08-06 16:54:41 +0200

    only for test

--- src/js/2-gallery.js
+++ src/js/2-gallery.js
@@ -11,3 +11,18 @@ function saveToLocalStorage() {
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 };
 
+function updateFormData(event) {
+    const { name, value } = event.target;
+    formData[name] = value;
+    saveToLocalStorage;
+};
+
+function returningValuesFromLocalStorageInInput() {
+    const savedData = localStorage.getItem('feedback-form-state');
+    if (savedData) {
+        const { email, message } = JSON.parse(savedData);
+        formData.email = email;
+        formData.message = message;
+        email
+    }
+}
